75ad10255e5b353907c3e0dc1b0b9fd9466df6d8
echow626 <elisse.chow@gmail.com>
2024-03-07 15:57:01 -0800

    uhhhh fix?
    - going to do more preprocessing :D

--- pose_detection.py
+++ pose_detection.py
@@ -3,34 +3,54 @@ import numpy as np
 import json
 from hog import *
 
-X_train, y_train = [], []
-X_val, y_val = [], []
-X_test, y_test = [], []
+X_train = []
+y_train = []
+X_val = []
+y_val = []
+X_test = []
+y_test = []
 
-def load_data():
-    # Load training dataset
-    with open("filtered_data/single_person_train.json", "r") as read_file:
+def preprocess_data():
+    global X_train, y_train, X_val, y_val, X_test, y_test
+
+    with open("filtered_data/single_person_small_train.json", "r") as read_file:
         train_images = json.load(read_file)
     for image_obj in train_images:
-        X_train.append(hog_feature(image_obj["image"]))
+        X_train.append(hog_feature("images/" + image_obj["image"]))
         y_train.append(np.ravel(image_obj["joints"]))
     X_train = tf.convert_to_tensor(tf.keras.utils.pad_sequences(X_train))
-    
-    # Load test dataset
-    with open("filtered_data/single_person_test.json", "r") as read_file:
-        test_images = json.load(read_file)
-    for image_obj in test_images:
-        X_test.append(hog_feature(image_obj["image"]))
-        y_test.append(np.ravel(image_obj["joints"]))
-    X_test = tf.convert_to_tensor(tf.keras.utils.pad_sequences(X_test))
 
-    # Load validation dataset
-    with open("filtered_data/single_person_valid.json", "r") as read_file:
-        val_images = json.load(read_file)
-    for image_obj in val_images:
-        X_val.append(hog_feature(image_obj["image"]))
-        y_val.append(np.ravel(image_obj["joints"]))
-    X_val = tf.convert_to_tensor(tf.keras.utils.pad_sequences(X_val))
+    print("Finished loading in training data")
+
+    # # Load training dataset
+    # with open("filtered_data/single_person_train.json", "r") as read_file:
+    #     train_images = json.load(read_file)
+    # for image_obj in train_images:
+    #     X_train.append(hog_feature("images/" + image_obj["image"]))
+    #     y_train.append(np.ravel(image_obj["joints"]))
+    # X_train = tf.convert_to_tensor(tf.keras.utils.pad_sequences(X_train))
+
+    # print("Finished loading in training data")
+    
+    # # Load test dataset
+    # with open("filtered_data/single_person_test.json", "r") as read_file:
+    #     test_images = json.load(read_file)
+    # for image_obj in test_images:
+    #     X_test.append(hog_feature("images/" + image_obj["image"]))
+    #     y_test.append(np.ravel(image_obj["joints"]))
+    # X_test = tf.convert_to_tensor(tf.keras.utils.pad_sequences(X_test))
+
+    # print("Finished loading in test data")
+
+    # # Load validation dataset
+    # with open("filtered_data/single_person_valid.json", "r") as read_file:
+    #     val_images = json.load(read_file)
+    # for image_obj in val_images:
+    #     X_val.append(hog_feature("images/" + image_obj["image"]))
+    #     y_val.append(np.ravel(image_obj["joints"]))
+    # X_val = tf.convert_to_tensor(tf.keras.utils.pad_sequences(X_val))
+
+    # print("Finished loading in validation data")
 
 def model():
     feature_length = None  # determine this from hog
