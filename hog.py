06cb42e05f69b76831ea3f573913a5bc8b5bef81
Caroline Zeng <caroline.f.zeng@gmail.com>
2024-03-09 14:05:37 -0800

    updated hog
     - process_image to visualize hog
     - hog_feature with cell size that is 1/25 of the length of the shortest dimension

--- hog.py
+++ hog.py
@@ -13,21 +13,20 @@ import time
 import warnings
 warnings.filterwarnings('ignore')
 
-def hog_feature(image_path, pixel_per_cell=8):
-    # hog_feature, hog_image = feature.hog(image, pixels_per_cell=(pixel_per_cell, pixel_per_cell), block_norm='L1', visualize='True', feature_vector='True', transform_sqrt='True')
-    # return hog_feature, hog_image
+def hog_feature(image_path):
     image = io.imread(image_path, as_gray=True)
+    pixel_per_cell = min(image.shape[0]/25,image.shape[1]/25)
     hog_feature = feature.hog(image, pixels_per_cell=(pixel_per_cell, pixel_per_cell), block_norm='L1', feature_vector='True', transform_sqrt='True')
     return hog_feature
 
-# def process_image(image_path='008963454_copy.jpg'):
-	# image = io.imread(image_path, as_gray=True)
-    # return hog_feature(image)
-	# hog_image_rescaled = exposure.rescale_intensity(hog_img, in_range=(0, 0.02))
-	# print(hog_ft)
-	# print(hog_ft.shape)
-	# plt.imshow(hog_image_rescaled)
-	# plt.show()
-    
+def process_image(image_path='008963454_copy.jpg'):
+	image = io.imread(image_path, as_gray=True)
+	pixel_per_cell = min(image.shape[0]/25, image.shape[1]/25)
+	hog_ft, hog_img = feature.hog(image, pixels_per_cell=(pixel_per_cell, pixel_per_cell), block_norm='L1', visualize='True', feature_vector='True', transform_sqrt='True')
+	print(hog_ft)
+	print(hog_ft.shape)
+	hog_image_rescaled = exposure.rescale_intensity(hog_img, in_range=(0, 0.02))
+	plt.imshow(hog_image_rescaled)
+	plt.show()
 
-# process_image('063271031_copy.jpg')
+process_image('082062225_copy.jpg')
