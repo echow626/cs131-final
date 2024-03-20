9533e6e1437f5ff414b304bbea2703ddffa9c08f
echow626 <elisse.chow@gmail.com>
2024-03-19 21:52:20 -0700

    asdfjaisdjfa

--- hog.py
+++ hog.py
@@ -15,15 +15,6 @@ warnings.filterwarnings('ignore')
 
 def hog_feature(image_path):
     image = io.imread(image_path, as_gray=True)
-    # pad_height = max(0, 1080 - image.shape[1])
-    # pad_width = max(0, 1920 - image.shape[0])
-    # pad_top = pad_height // 2
-    # pad_bottom = pad_height - pad_top
-    # pad_left = pad_width // 2
-    # pad_right = pad_width - pad_left
-    # padded_image = np.pad(image, ((pad_top, pad_bottom), (pad_left, pad_right), (0, 0)), mode='constant')
-    # resized = resize(image, (1080, 1920), mode='constant')
-    # ^ does not work/couldn't figure it out T^T
     pixel_per_cell = min(image.shape[0]/25, image.shape[1]/25)
     hog_feature = feature.hog(image, pixels_per_cell=(pixel_per_cell, pixel_per_cell), block_norm='L1', feature_vector='True', transform_sqrt='True')
     return hog_feature
@@ -37,5 +28,6 @@ def process_image(image_path='008963454_copy.jpg'):
 	hog_image_rescaled = exposure.rescale_intensity(hog_img, in_range=(0, 0.02))
 	plt.imshow(hog_image_rescaled)
 	plt.show()
+      
 
 # process_image('008963454_copy.jpg')
